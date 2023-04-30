a493dfad689f57d8ad56fb7d0f7ef902ca0f759c
000raihan <000raihanahmad@gmail.com>
2023-04-30 22:42:24 +0600

    updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api', { target: 'http://116.68.200.97:7030' }));
+    app.use(createProxyMiddleware('/api', { target: 'http://localhost:7030' }));
+    // app.use(createProxyMiddleware('/api', { target: 'http://116.68.200.97:7030' }));
     // app.use(createProxyMiddleware('/reports', { target: 'http://127.0.0.1:7003/api/v1' }));
 };
