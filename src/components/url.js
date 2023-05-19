aff578c52c0faebf3bb4f75fdf8545c2f9ed9b0c
000raihan <000raihanahmad@gmail.com>
2023-05-20 01:14:39 +0600

    updated

--- src/components/url.js
+++ src/components/url.js
@@ -1,2 +1,2 @@
-// export const basr_url = "http://116.68.200.97:6054";
-export const basr_url = "http://localhost:6054";
+export const basr_url = "http://116.68.200.97:6054";
+// export const basr_url = "http://localhost:6054";
