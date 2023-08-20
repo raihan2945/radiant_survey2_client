3a9f0ebf47aa29dab91bff831ca7da954ee4bb3e
000raihan <000raihanahmad@gmail.com>
2023-08-20 13:15:31 +0600

    updated

--- src/components/survey_3/FirstPart.js
+++ src/components/survey_3/FirstPart.js
@@ -19,7 +19,7 @@ const FirstPart = ({ setPart, name, setName, mio_code, setMio_code }) => {
 
      </div>
       <div style={{ textAlign: 'center', marginBottom:"1rem"}}>
-      <img style={{}} src="/images/naprosyn_logo.png"/>
+      <img style={{width:"90%", objectFit:"contain"}} src="/images/naprosyn_logo.png"/>
         <h1 style={{margin:0, textAlign: "center", fontSize: "20px", color: 'white', fontWeight: "700" }}>WISH FOR BANGLADESH NATIONAL CRICKET TEAM</h1>
       </div>
       <div
